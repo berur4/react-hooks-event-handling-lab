edbdb7aecf1cb711f68da086557400a15024e390
berur4 <berurcaleb@gmail.com>
2024-11-30 21:05:16 +0300

    event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
-// Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+
+function Keypad() {
+  
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input 
+        type="password" 
+        onChange={handleChange} 
+        placeholder="Enter password" 
+        aria-label="Password input"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
